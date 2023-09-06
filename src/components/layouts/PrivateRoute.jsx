73597d4013f0e871d09b994c3f3cac02bdf20798
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-06 14:26:45 +0600

    add: User persistency

--- src/components/layouts/PrivateRoute.jsx
+++ src/components/layouts/PrivateRoute.jsx
@@ -1,11 +1,31 @@
-import { Navigate, useLocation } from 'react-router-dom';
-import Loading from './Loading';
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "./Loading";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "../../utils/firebase.config";
+import { setUser, toggleLoading } from "../../redux/features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const PrivateRoute = ({ children }) => {
   const { pathname } = useLocation();
-
-  const isLoading = false;
-  const email = 'test@gmail.com';
+  const { email, isLoading } = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          setUser({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+          })
+        );
+        dispatch(toggleLoading(false));
+      } else {
+        dispatch(toggleLoading(false));
+      }
+    });
+  }, []);
 
   if (isLoading) {
     return <Loading />;
--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,14 +1,64 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import auth from "../../../utils/firebase.config";
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: "",
+  email: "",
+  isLoading: true, // Change this to false initially
+  isError: false,
+  error: "",
 };
 
-const userSlice = createSlice({
-  name: 'userSlice',
-  initialState,
-  reducers: {},
-});
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  async ({ name, email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+    console.log(data);
+    return {
+      name: data.user.displayName,
+      email: data.user.email,
+    };
+  }
+);
 
+const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.name = payload.name;
+      state.email = payload.email;
+    },
+    toggleLoading: (state, { payload }) => {
+      state.isLoading = payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createUser.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.error = "";
+      state.name = "";
+      state.email = "";
+    });
+    builder.addCase(createUser.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.name = payload.name;
+      state.email = payload.email;
+    });
+    builder.addCase(createUser.rejected, (state, { payload, error }) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.error = error.message; // Access the error message from payload
+      state.name = "";
+      state.email = "";
+    });
+  },
+});
+export const { setUser, toggleLoading } = userSlice.actions;
 export default userSlice.reducer;
